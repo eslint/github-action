8aebaa093bb7d42675e66b3848b9175906c0911b
Nicholas C. Zakas <nicholas@nczconsulting.com>
2019-09-23 10:42:33 -0700

    New: Add in stub calls to Checks API

--- src/index.js
+++ src/index.js
@@ -1,18 +1,53 @@
-const core = require('@actions/core');
+/**
+ * @fileoverview Main GitHub Action file
+ * @author Nicholas C. Zakas
+ */
 
+//-----------------------------------------------------------------------------
+// Requirements
+//-----------------------------------------------------------------------------
+
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+//-----------------------------------------------------------------------------
+// Main
+//-----------------------------------------------------------------------------
 
 // most @actions toolkit packages have async methods
 async function run() {
-  try {
-    core.debug((new Date()).toTimeString())
+    try {
 
-    const token = core.getInput("githubToken", { required: true });
+        const token = core.getInput("githubToken", { required: true });
+        const octokit = new github.GitHub(token);
+        const context = github.context;
 
-    // do something with token
+        if (context.eventName !== "pull_request") {
+            core.setFailed("ESLint GitHub Action can only be performed on the pull_request event.");
+            return;
+        }
 
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+        const { data: { checkId } } = await octokit.checks.create({
+            ...context.repo,
+            name: "@eslint/github-action",
+            head_sha: context.sha,
+            status: "in_progress"
+        });
+
+        await octokit.checks.update({
+            ...context.repo,
+            check_run_id: checkId,
+            status: "completed",
+            conclusion: "success",
+            output: {
+                title: "ESLint Check",
+                summary: "No problems found."
+            }
+        });
+
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
 
 run()
